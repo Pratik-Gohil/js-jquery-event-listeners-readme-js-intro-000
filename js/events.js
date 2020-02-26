e5e12c96e2b40a5a8c65d4b451b4ec8c975f36d3
Pratik <pr.tikgohil@gmail.com>
2020-02-26 17:49:40 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,11 @@
 function getIt() {
   $('p').on('click', () => alert("Hey!"))
 }
+function frameIt() {
+  $('img').on('load' () => {
+    $('img').addClass('tasty')
+  })
+}
 
 $(document).ready(function(){
 
