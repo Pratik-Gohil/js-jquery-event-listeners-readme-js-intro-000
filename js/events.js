0250b2dc753f88af106625d57f158d5e29d1c648
Pratik <pr.tikgohil@gmail.com>
2020-02-26 18:32:56 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,11 +1,18 @@
 //define functions here
 function getIt() {
-  $('p').on("click", () => alert("Hey!"))
+  $("p").on("click", () => alert("Hey!"))
 }
 function frameIt(){
   $("img").on("load", function(){
     $("img").addClass("tasty");
   });
+}
+function pressIt() {
+  $("input").bind("keydown", (e) => {
+    if (event.which == 72) {
+      alert("G")
+    }
+  })
 }
 
 $(document).ready(function(){
